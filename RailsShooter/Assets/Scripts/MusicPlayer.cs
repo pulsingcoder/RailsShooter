7b0f4f7b0d3b789d626f9d974e80264c3edb3541
Yuvraj Darwani <44190710+pulsingcoder@users.noreply.github.com>
2020-03-15 16:22:53 +0530

    SplashScreenSetup

---
+++ RailsShooter/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        Invoke("LoadFirstScene", 3f);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
