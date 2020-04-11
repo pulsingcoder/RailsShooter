e417c2c67bb74d906abeca1525e438bfe778dd9b
Yuvraj Darwani <44190710+pulsingcoder@users.noreply.github.com>
2020-04-11 21:29:58 +0530

    Singleton

--- RailsShooter/Assets/Scripts/CollisionHandler.cs
+++ RailsShooter/Assets/Scripts/CollisionHandler.cs
@@ -2,20 +2,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    
+    [SerializeField] GameObject deathFX;
+    [SerializeField] float levelLoadDelay = 1f;
+
     private void OnTriggerEnter(Collider other)
     {
-       
+        deathFX.SetActive(true);
         StartDeathSequence();
+        Invoke("ReloadScene", levelLoadDelay);
     }
 
     private void StartDeathSequence()
     {
-        print("Player dying");
+       
         SendMessage("OnPlayerDeath");
 
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(1);
     }
 }
--- RailsShooter/Assets/Scripts/MusicPlayer.cs
+++ RailsShooter/Assets/Scripts/MusicPlayer.cs
@@ -7,7 +7,15 @@ public class MusicPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+        if (numMusicPlayers > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
     }
     
    
