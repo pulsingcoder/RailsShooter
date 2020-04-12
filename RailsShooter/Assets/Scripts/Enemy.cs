6841ae3afd104b03ffc8af0bf1c03b08c83bf95b
Yuvraj Darwani <44190710+pulsingcoder@users.noreply.github.com>
2020-04-12 12:38:21 +0530

    ParticleCollision

---
+++ RailsShooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
---
+++ RailsShooter/Assets/Scripts/SelfDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 5f); // todo customisation
+    }
+
+    
+}
